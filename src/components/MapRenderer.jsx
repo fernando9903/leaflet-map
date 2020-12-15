92cb9f1a4fa713bdd941053a078248ea20becbae
Kledioz <claudiobo1122@gmail.com>
2020-12-15 04:39:06 -0700

    fix 2 and old popup

--- src/components/MapRenderer.jsx
+++ src/components/MapRenderer.jsx
@@ -14,6 +14,7 @@ export default class MapRenderer extends Component {
   onEachCountry = (country, layer) => {
     const countryName = country.properties.ADMIN;
     // layer.bindPopup(ReactDOMServer.renderToString(<ChartTest/>));
+    layer.bindPopup("*insert chart here*");
     layer.on({
       click: this.clickChangeStyle,
     });
@@ -58,7 +59,7 @@ export default class MapRenderer extends Component {
       >
 
         <LayersControl position="topleft">
-          <BaseLayer checked name="Land cover in 2010 from ESA CCI">
+          <BaseLayer name="Land cover in 2010 from ESA CCI">
             <TileLayer
               url="http://158.69.121.180/fablemap-testing/tifs/ESACCI-LC-L4-LCCS-Map-300m-P1Y-2010-v2.0.7/{z}/{x}/{y}.png"
               attribution='&amp;copy (ESACCI-LC-L4-LCCS-Map-300m-P1Y-2010-v2.0.7), Country names Tiles by <a href="http://stamen.com">Stamen Design</a>'
